e3d4a375af489137fe0f49fce4210bf0bd0d40b9
sam <sam@mac.mynetworksettings.com>
2026-08-12 13:18:52 -0400

    add program generation

--- simpleweb/www_google_com/http___www_google_com_/matcher.js
+++
@@ -1,9 +0,0 @@
-
-(function() {
-    var hasForm = document.querySelector('form') !== null;
-    var hasSearchInput = document.querySelector('[name="q"]') !== null;
-    var hasSearchButtons = document.querySelector('[name="btnK"], [name="btnI"]') !== null;
-    var hasNavOrFooterLinks = document.querySelector('a.w5hRs, a.gb_6, a.gb_A, .pHiOh') !== null;
-
-    return hasForm && hasSearchInput && hasSearchButtons && hasNavOrFooterLinks;
-})();
